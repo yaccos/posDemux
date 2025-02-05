2b1249240fca7fea0711f6878676186e0382c260
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2025-02-05 17:10:46 +0100

    Resolved a package loading error

---
+++ R/posDemux-package.R
@@ -0,0 +1,9 @@
+#' @name posDemux
+#' 
+#' @useDynLib posDemux, .registration = TRUE
+#' 
+#' @docType package
+#' 
+#' @author Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
+#' 
+"_PACKAGE"
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -25,20 +25,9 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// rcpp_hello_world
-List rcpp_hello_world();
-RcppExport SEXP _posDemux_rcpp_hello_world() {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(rcpp_hello_world());
-    return rcpp_result_gen;
-END_RCPP
-}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_posDemux_hamming_match", (DL_FUNC) &_posDemux_hamming_match, 5},
-    {"_posDemux_rcpp_hello_world", (DL_FUNC) &_posDemux_rcpp_hello_world, 0},
     {NULL, NULL, 0}
 };
 
