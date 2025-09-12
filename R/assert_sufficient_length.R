a48e84454ad52bbec1ba189df471f1305efd8741
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2025-09-12 15:52:30 +0200

    Corrected a minor syntax error, bumped package version

--- R/assert_sufficient_length.R
+++ R/assert_sufficient_length.R
@@ -9,7 +9,7 @@ assert_sufficient_length <- function(sequences, minimum_length) {
   # One or more sequences are not long enough
   n_too_short <- sum(!sequence_long_enough)
   head_msg <- glue("{n_too_short} sequence(s) are too short given the provided lengths of the segments\n")
-  rlang::is_named(sequences){
+  if(rlang::is_named(sequences)) {
     # We print their names if they exist
     tail_msg <- glue("These are:")
     names_too_short <- names(sequences)
